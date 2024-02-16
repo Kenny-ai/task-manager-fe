608643651ef69b6f23b287922a26983c079649fd
Kenny-ai <kennysoyoye07@gmail.com>
2024-02-17 00:15:38 +0100

    ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from "next/font/google";
+import { Plus_Jakarta_Sans, Inter } from "next/font/google";
 import Providers from "../providers/Providers";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const plusjakartasans = Plus_Jakarta_Sans({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kanban Task Management App",
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${plusjakartasans.className} bg-light-main-bg dark:bg-dark-main-bg`}
+        className={`${inter.className} bg-light-main-bg dark:bg-dark-main-bg`}
       >
         <Providers>
           <ToastContainer
